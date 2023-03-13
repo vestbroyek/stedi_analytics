3e24cba6219e43e5e75d9286ae88931d5577c82d
Maurits <mcb.westbroek@gmail.com>
2023-03-13 08:21:50 +0000

    Simply raw data folder structure

---
+++ data/s3-sync-all.sh
@@ -0,0 +1,3 @@
+aws s3 sync ./data/customers/ s3://stedi-human-balance-analytics-mw/customer/landing
+aws s3 sync ./data/accelerometer/ s3://stedi-human-balance-analytics-mw/accelerometer/landing
+aws s3 sync ./data/step_trainer/ s3://stedi-human-balance-analytics-mw/step_trainer/landing
